a9b77a9e7a2ec602e7a54b3d9d62e1499a2519b5
Daniel Fages <dan@androvm.org>
2013-08-30 18:21:44 +0200

    Don't destroy OpenGL context if current

--- emulator/opengl/system/egl/egl.cpp
+++ emulator/opengl/system/egl/egl.cpp
@@ -901,7 +901,9 @@ EGLBoolean eglDestroyContext(EGLDisplay dpy, EGLContext ctx)
 
     if (getEGLThreadInfo()->currentContext == context)
     {
-        eglMakeCurrent(dpy, EGL_NO_CONTEXT, EGL_NO_SURFACE, EGL_NO_SURFACE);
+        // eglMakeCurrent(dpy, EGL_NO_CONTEXT, EGL_NO_SURFACE, EGL_NO_SURFACE);
+        ALOGE("egl: destroying current context refused\n");
+        return EGL_FALSE;
     }
 
     if (context->rcContext) {
